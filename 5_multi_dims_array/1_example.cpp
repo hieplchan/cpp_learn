12b46cddd4b910fce2f604387021bc02902d6644
Lương Chấn Hiệp <v.hieplc@vinai.io>
2021-07-21 17:24:38 +0700

    add multi dims array example

---
+++ 5_multi_dims_array/1_example.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main() {
+    constexpr size_t row = 3, col = 4;
+    int ia[row][col];
+
+    // for (size_t i = 0; i != row; ++i) {
+    //     for (size_t j = 0; j != col; ++j) {
+    //         ia[i][j] = i*col + j;
+    //     }
+    // }
+
+    // Another version using for range
+    int cnt = 0;
+    for (auto &row : ia)
+        for (auto &col : row) {
+            col = cnt;
+            ++cnt;
+        }
+
+    for (auto &row : ia)
+        for (auto &col : row)
+            std::cout << col << " ";
+
+    std::cout << std::endl;
+
+    return 0;
+}
