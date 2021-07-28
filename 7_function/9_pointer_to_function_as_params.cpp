57d4c901e38e58664f35a7efb850881eaf919767
Lương Chấn Hiệp <v.hieplc@vinai.io>
2021-07-28 16:00:15 +0700

    using type alias for pointer to func - clear to see

--- 7_function/9_pointer_to_function_as_params.cpp
+++ 7_function/9_pointer_to_function_as_params.cpp
@@ -12,10 +12,15 @@ string::size_type lengthSum(const string &s1, const string &s2) {
     return s1.size() + s2.size();
 }
 
+// Type alias
+// typedef decltype(lengthCompare) callbackFunc;
+using callbackFunc = decltype(lengthCompare); // same as above
+
 // void testFunc(const string &s1, const string &s2, string::size_type pf(const string &, const string &)) { 
-void testFunc(const string &s1, const string &s2, string::size_type (*pf)(const string &, const string &)) { // same as above
+// void testFunc(const string &s1, const string &s2, string::size_type (*pf)(const string &, const string &)) { // same as above
+void testFunc(const string &s1, const string &s2, callbackFunc *pf) { // same as above but clear to see
     cout << "testFunc" << endl;
-    string::size_type result1 = pf(s1, s2); // call lengthCompare
+    string::size_type result1 = pf(s1, s2); // call lengthCompare/lengthSum
     cout << result1 << endl;
 }
 
