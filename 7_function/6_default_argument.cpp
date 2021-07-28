60d9f2540776c07037c9b6908d29e77f5beadb70
Lương Chấn Hiệp <v.hieplc@vinai.io>
2021-07-28 10:00:26 +0700

    add inline to func - optimize small func that called frequently

--- 7_function/6_default_argument.cpp
+++ 7_function/6_default_argument.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void printSomething(char arg1 = '1', char arg2 = '2', char arg3 = '3') {
+void inline printSomething(char arg1 = '1', char arg2 = '2', char arg3 = '3') {
     cout << "printSomething: " << arg1 << arg2 << arg3 << endl;
 }
 
