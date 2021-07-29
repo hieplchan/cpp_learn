20dcc458089f92d32a2fc0a914340720292c637d
Lương Chấn Hiệp <v.hieplc@vinai.io>
2021-07-29 17:37:54 +0700

    add implement for Sales_data class

--- 8_classes/1_simple_example.cpp
+++ 8_classes/1_simple_example.cpp
@@ -3,12 +3,28 @@
 using std::cout;
 using std::cin;
 using std::endl;
+using std::cerr;
 
 int main() {
     cout << "Sales_data class simple example" << endl;
     cout << "Size of Sales_data object: " << sizeof(Sales_data) << " bytes" << endl;
 
-    Sales_data data1;
-
+    Sales_data total;
+    if (read(cin, total)) {
+        Sales_data trans;
+        while (read(cin, trans))
+        {
+            if (total.isbn() == trans.isbn()) {
+                total.combine(trans);
+            } else {
+                print(cout, total) << endl;
+                total = trans;
+            }
+        }
+        print(cout, total) << endl;
+    } else {
+        cerr << "No data ?!" << endl;
+    }
+    
     return 0;
 }
--- 8_classes/Sales_data.h
+++ 8_classes/Sales_data.h
@@ -9,7 +9,7 @@ struct Sales_data {
     // const at the end - compiler throw error if this function change member variable of class
     // function defined inside class are implicity inline
     std::string isbn() const { return m_book_no; } 
-    Sales_data& combine(const Sales_data&) {} // declare inside class but can be defined elsewhere
+    Sales_data& combine(const Sales_data&); // declare inside class but can be defined elsewhere
     double avg_price() const; // part of implement - not interface
 
     /* data member of class Sales_data */
@@ -19,8 +19,44 @@ struct Sales_data {
 };
 
 /* nonmember Sales_data interface function */
-Sales_data add(const Sales_data &data1, const Sales_data &data2);
-std::ostream &print(std::ostream &out_stream, const Sales_data &data);
-std::istream &read(std::istream &in_stream, const Sales_data &data);
+Sales_data add(const Sales_data&, const Sales_data&);
+std::ostream &print(std::ostream&, const Sales_data&);
+std::istream &read(std::istream&, Sales_data&);
+
+/* Function definition - implementation */
+
+/* member of Sales_data but defined outside of the Class */
+double Sales_data::avg_price() const { // must have Sales_data::
+    if (m_units_sold) {
+        return m_revenue/m_units_sold;
+    } else {
+        return 0;
+    }
+}
+
+Sales_data& Sales_data::combine(const Sales_data &data) {
+    m_units_sold += data.m_units_sold;
+    m_revenue += data.m_revenue;
+    return *this; // return object on which function was call
+}
+
+std::istream &read(std::istream &in_stream, Sales_data &item) {
+    double price = 0;
+    in_stream >> item.m_book_no >> item.m_units_sold >> price;
+    item.m_revenue = item.m_units_sold * price;
+    return in_stream;
+}
+
+std::ostream &print(std::ostream &out_stream, const Sales_data &item) {
+    out_stream << item.isbn() << " " << item.m_units_sold << " "
+               << item.m_revenue << " " << item.avg_price(); // Let user decide if they need new line or not
+    return out_stream;
+}
+
+Sales_data add(const Sales_data &item1, const Sales_data &item2) {
+    Sales_data sum = item1; // new sum is copy of item1
+    sum.combine(item2);
+    return sum; // return copy of sum
+}
 
 #endif
