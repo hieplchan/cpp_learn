c30ab2982cb85cc0c2a20ec8bda776254104d4c7
hiep.lchan <hiep.lchan@gmail.com>
2025-02-09 20:57:50 +0700

    add simple fold expression

--- a_tour_of_cpp/7_Generic_Programming/Variadic_Template.cpp
+++ a_tour_of_cpp/7_Generic_Programming/Variadic_Template.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std::literals::string_literals;
 
@@ -24,7 +25,49 @@ void user()
     print("\nsecond: ", 0.2, 'c', "yuck!"s, 0, 1, 2, '\n');
 }
 
+#pragma region Fold Expression
+
+template<typename... T>
+int sum(T... v)
+{
+    return (v + ... + 0); // (v[0]+(v[1]+(v[2]+(v[3]+(v[4]+0)))))
+    // return (0 + ... + v); // (((((0+v[0])+v[1])+v[2])+v[3])+v[4])
+}
+
+template<typename... T>
+void printT(T&&... args)
+{
+    (std::cout << ... << args) << '\n';
+}
+
+template<typename Res, typename... Ts>
+std::vector<Res> to_vector(Ts&&... ts)
+{
+    std::vector<Res> res;
+    (res.push_back(ts), ...); // no init value needed
+    return res;
+}
+
+template<typename... Ts>
+void fct(Ts&&... ts)
+{
+    auto args = to_vector<std::string>(ts...);
+    // further use args
+}
+
+void useToVec()
+{
+    // can use to simple copy args to vector for further use
+    auto x = to_vector<double>(1, 2, 4.5, 'a');
+    fct("foo", "bar");
+}
+
+#pragma endregion
+
 int main()
 {
     user();
+    std::cout << sum(1, 2, 3) << std::endl;
+    printT("Hello!"s, ' ', "World ", 2017);
+    useToVec();
 }
